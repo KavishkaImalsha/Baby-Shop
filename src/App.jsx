cb2ffbeabb5f12c4474fd48f03c937f9920cbe7c
Kavishka <kavishkaimalsha@gmail.com>
2024-11-04 23:48:48 +0530

    Add about page rote

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import RoutePages from "./navigration/RoutePages"
 import Home from "./pages/Home"
 import Products from "./pages/Products"
 import Contact from "./pages/Contact"
+import About from "./pages/About"
 
 const App = () => {
     return(
@@ -13,6 +14,7 @@ const App = () => {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/products" element={<Products/>}/>
                 <Route path="/contact" element={<Contact/>}/>
+                <Route path="/about" element={<About/>}/>
             </Routes>
         </>
     )
--- src/components/navbars/NavBar.jsx
+++ src/components/navbars/NavBar.jsx
@@ -36,6 +36,7 @@ const NavBar = () => {
                         <DropDown isVisible={isVisible} />
                     </li>
                     <li>
+                    <a href="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to="/contact">Contact</Link></a>
                     </li>
                     <li>
                     <a href="/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><Link to="/contact">About us</Link></a>
